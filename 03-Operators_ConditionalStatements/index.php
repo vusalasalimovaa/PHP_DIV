09af3c19f113572f9b0024ac44d019c36148db61
vusalasalimovaa <salimovavusala7@gmail.com>
2025-12-10 21:01:45 +0400

    php

---
+++ 03-Operators_ConditionalStatements/index.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * $_GET
+ * $_POST
+ * $_FILES
+ * $_SERVER
+ */
+
+/**
+ * Aritmetik
+ * +
+ * -
+ * *
+ * /
+ * %
+ */
+
+/**
+ * =
+ * .=
+ * .
+ * +=
+ * -=
+ * *=
+ * /=
+ * %=
+ */
+
+/**
+ * && and
+ * || or
+ */
+
+echo "salam" . ", php";
+
+$cumle = "";
+$cumle .= "salam ";
+$cumle .= "php";
+
+echo "\n";
+echo $cumle;
+
+//  true ?? false
+
+$user = [
+    'name' => 'John'
+];
+
+echo $user['surname'] ?? 'yoxdur';
+
+// if ( array_key_exists('surname', $user) ) {
+//     echo $user['surname'];
+// } else {
+    
+// }
--- 04-ConditionalStatements/index.php
+++ 04-ConditionalStatements/index.php
@@ -1,62 +1,6 @@
 <?php
 
 /**
- * $_GET
- * $_POST
- * $_FILES
- * $_SERVER
- */
-
-/**
- * Aritmetik
- * +
- * -
- * *
- * /
- * %
- */
-
-/**
- * =
- * .=
- * .
- * +=
- * -=
- * *=
- * /=
- * %=
- */
-
-/**
- * && and
- * || or
- */
-
-echo "salam" . ", php";
-
-$cumle = "";
-$cumle .= "salam ";
-$cumle .= "php";
-
-echo "\n";
-echo $cumle;
-
-//  true ?? false
-
-$user = [
-    'name' => 'John'
-];
-
-echo $user['surname'] ?? 'yoxdur';
-
-// if ( array_key_exists('surname', $user) ) {
-//     echo $user['surname'];
-// } else {
-    
-// }
-
-
-/**
  * if / else if /else
  * switch / case
  * match
